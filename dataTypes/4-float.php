a0fb0369413e35a6dcef4d614e5cdafc8d1b6858
TOSHIBA <TOSHIBA@TOSHIBA-pc>
2014-01-25 18:50:18 +0200

    Dersler commit ediliyor...

---
+++ dataTypes/4-float.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @link http://www.php.net/manual/en/language.types.float.php Detaylı bilgi
+ */
+
+$a = 35.99;
+
+$b = 1.2e3; //1.2 x 10 üstü 3
+
+$c = 7e-10; // 7 x 10 üstü -10
+
+
+echo $c;
